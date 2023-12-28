4a196f2c3a1b8b46312b8f39478870205c21f90a
alex-belokon <council.belokon1988@gmail.com>
2023-12-28 17:47:57 +0100

    Init commit. Implement sidebar + styles

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Javascript Tutorial',
+        productNumber: '100234',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '200957',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '300724',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    }
+]
